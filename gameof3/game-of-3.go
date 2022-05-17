81b71191c332f2c41eb11f8a8507ffa0004976ad
catarina.bombaca <catarinaipbombaca@gmail.com>
2022-05-17 20:34:42 +0100

    refactor tests and add edge cases

--- gameof3/game-of-3.go
+++ gameof3/game-of-3.go
@@ -5,15 +5,20 @@ func Execute(number int64) []int64 {
 
 	for number != 1 {
 		switch {
+		case number == 0:
+			number = number+1
 		case number % 3 == 0:
 			number = number/3
 		case (number - 1) % 3 == 0:
 			number = number-1
 		case (number + 1) % 3 == 0:
 			number = number+1
+		default:
+			number = number+1
 		}
 
 		result = append(result, number)
 	}
+
 	return result
 }
--- gameof3/game-of-3_test.go
+++ gameof3/game-of-3_test.go
@@ -9,11 +9,22 @@ import (
 func TestGameof3(t *testing.T) {
 	var (
 		assert = assert.New(t)
-		num int64 = 100
-		expectedResult = []int64{100, 99, 33, 11, 12, 4, 3, 1}
+		tests = []test{
+			{100, []int64{100, 99, 33, 11, 12, 4, 3, 1}},
+			{1, []int64{1}},
+			{0, []int64{0,1}},
+			{-1, []int64{-1,0,1}},
+			{-40, []int64{-40, -39, -13, -12, -4, -3, -1, 0, 1}},
+		}
 	)
 
-	result := gameof3.Execute(num)
+	for _, test := range tests {
+	result := gameof3.Execute(test.num)
+	assert.Equal(test.expectedResult, result)
+	}
+}
 
-	assert.Equal(expectedResult, result)
+type test struct {
+	num int64
+	expectedResult []int64
 }
