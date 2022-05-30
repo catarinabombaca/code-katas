bedde27023c20c7dca3a5b0026b0ccb44438cfc1
catarina.bombaca <catarinaipbombaca@gmail.com>
2022-05-30 09:41:48 +0100

    refactor letter sum kata

--- lettersum/lettersum.go
+++ lettersum/lettersum.go
@@ -3,12 +3,16 @@ package lettersum
 import "strings"
 
 func Execute(input string) int {
-	runes := []rune(strings.ToLower(input))
+	lettersAsValues := []rune(strings.ToLower(input))
+	sum := sumAllValues(lettersAsValues)
 
+	return sum
+}
+
+func sumAllValues(lettersAsValues []rune) int {
 	sum := 0
-	for _, r := range runes {
-		sum += int(r)-96
+	for _, value := range lettersAsValues {
+		sum += int(value) - 96
 	}
-
 	return sum
 }
